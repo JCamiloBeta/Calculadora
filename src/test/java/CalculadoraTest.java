d15e285d5a4c8207ed5d2b5d2c0bef477421758a
JCamiloBeta <jbetancurrico@gmail.com>
2021-08-19 20:55:00 -0500

    first commit

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @Test
+    @DisplayName("Add two numbers")
+    void add() {
+        assertEquals(4, Calculadora.add(2, 2));
+    }
+
+    @Test
+    @DisplayName("Multiply two numbers")
+    void multiply() {
+        assertAll(() -> assertEquals(4, Calculadora.multiply(2, 2)),
+                () -> assertEquals(-4, Calculadora.multiply(2, -2)),
+                () -> assertEquals(4, Calculadora.multiply(-2, -2)),
+                () -> assertEquals(0, Calculadora.multiply(1, 0)));
+    }
+}
